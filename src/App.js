cb787b20cc7a93d283d71d3e455d18e643e8cff0
jsxgod <smykoo7@gmail.com>
2021-01-07 20:17:12 +0100

    Adding weather icons and gradient backgrounds based on temperature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import {useState} from 'react';
+import Skycons, { SkyconsType } from 'react-skycons';
 
 const api = {
   key: 'b8b7f5d0fa269aa3f668fdacf2588b7c',
@@ -9,6 +10,9 @@ const api = {
 
 function App() {
 
+  const svgProps = {
+    style: { backgroundColor: '' },
+  }
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
@@ -19,23 +23,62 @@ function App() {
       .then(result => {
           setWeather(result);
           setQuery('');
-
-          console.log(result);
         });
+    }
+  };
+
+  const determineWeatherCSS = (temperature) => {
+    if (temperature > 18) {
+      return 'App warm';
+    } else if (temperature <= 0) {
+      return 'App freezing';
+    } else {
+      return 'App moderate';
+    }
+  }
+
+  const determineWeatherIcon = (weather) => {
+    const weatherType = weather.main;
+    const weatherDesc = weather.description;
+
+    if (weatherType === 'Thunderstorm'){
+      return SkyconsType.RAIN;
+    }
+    if (weatherType === 'Drizzle'){
+      return SkyconsType.SLEET;
+    }
+    if (weatherType === 'Rain'){
+      return SkyconsType.RAIN;
+    }
+    if (weatherType === 'Snow'){
+      return SkyconsType.SNOW;
+    }
+    if (weatherType === 'Clear'){
+      return SkyconsType.CLEAR_DAY;
+    }
+    if (weatherType === 'Clouds'){
+      if (weatherDesc === 'few clouds' || weatherDesc === 'scattered clouds'){
+        return SkyconsType.PARTLY_CLOUDY_DAY;
+      } else {
+        return SkyconsType.CLOUDY;
+      }
+    }
+    if (weatherType === 'Fog' || weatherType ==='Mist'){
+      return SkyconsType.FOG;
+    } else {
+      return SkyconsType.PARTLY_CLOUDY_DAY;
     }
   }
 
   const getDate = () => {
     let date = new window.Date().toDateString()
     return `${date}`;
-  }
+  };
 
   return (
     <div className={
       (typeof weather.main != "undefined") 
-      ? (weather.main.temp > 16 
-          ? 'App warm' 
-          : 'App') 
+      ? (determineWeatherCSS(weather.main.temp)) 
       : 'App'}>
       <main>
         <div className="search-box">
@@ -47,10 +90,8 @@ function App() {
             value={query}
             onKeyPress={search}
           >
-
           </input>
         </div>
-
         {(typeof weather.main != "undefined") ? (
           <div>
             <div className="location-box">
@@ -64,8 +105,21 @@ function App() {
                 <div className="temperature">
                   {Math.round(weather.main.temp)}°C
                 </div>
-                <div className="info">
+                <div className="icon">
+                  <Skycons
+                  color="white"
+                  type={determineWeatherIcon(weather.weather[0])}
+                  animate={true}
+                  size={50}
+                  resizeClear={true}
+                  {...svgProps}
+                />
+                 </div>
+                <div className="weather-type">
                   {weather.weather[0].main}
+                  <div className="weather-description">
+                  {weather.weather[0].main === 'Clouds' ? weather.weather[0].description : ''}
+                  </div>
                 </div>
               </div>
             </div>
